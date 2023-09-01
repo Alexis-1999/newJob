f8b95d7aaf124e2b9d14334de3e7e4e4321c1ae8
Troceitor <alexisaguilarg19@gmail.com>
2023-08-31 22:51:46 -0400

    Clase 4 Hooks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Contacto from "./pages/Contacto"
 import QuienSoy from "./pages/QuienSoy"
 // import Greeting from './components/pure/greeting';
 // import GreetingF from './components/pure/greetingF';
-import TaskListComponent from './components/container/task_list';
+// import TaskListComponent from './components/container/task_list';
+import Ejemplo1 from './hooks/Ejemplo1';
+import Ejemplo2 from './hooks/Ejemplo2';
+import MiComponenteConContexto from './hooks/Ejemplo3';
+import Ejemplo4 from './hooks/Ejemplo4';
 
 
 import './App.css';
@@ -89,7 +93,18 @@ function App() {
        <GreetingF name="Alexis"/> */}
 
        {/* Componente de Listado de Tareas */}
-       <TaskListComponent />
+       {/* <TaskListComponent /> */}
+
+       {/* Ejemplos de usro de HOOKS */}
+       {/* <Ejemplo1></Ejemplo1> */}
+       {/* <Ejemplo2></Ejemplo2> */}
+       {/* <MiComponenteConContexto></MiComponenteConContexto> */}
+       <Ejemplo4 nombre="Alexis">
+        {/* Todo lo que hay aquí, es tratado como props.children */}
+        <h3>
+          Contenido del props.children
+        </h3>
+       </Ejemplo4>
       </header>
     </div>
   );
